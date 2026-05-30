622be2ab265b40e0fd04dc91c41e23839f8ec214
Jhois Collado <rintohsaka3214@gmail.com>
2026-05-30 02:14:27 -0400

    Listado De Compras

---
+++ Tarea 2 (Listado De Compras)/Lista.cpp
@@ -0,0 +1,30 @@
+#include "Lista.h"
+
+Lista::Lista(): _primer(NULL) , _ultimo(NULL)
+{
+
+}
+
+void Lista::Agregar(Elemento* elemento)
+{
+	/*IMPLEMENTE ESTE METODO
+		Dado un objeto de tipo puntero a Elemento agréguelo a la lista.
+
+		Tenga pendiente que el atributo _primer guarda un puntero al primer elemento de la lista y que _ultimo es un puntero al al último elemento de la lista
+	*/
+
+}
+
+bool Lista::Eliminar(int indice)
+{
+	/*
+	IMPLEMENTE ESTE METODO
+	Dado un indice entero elimine el objeto en esa posición.  Ejemplo si el parámetro indice es igual a 0 se eliminará el primer elemento.
+
+	Esta función retorna un booleano,  retorno verdadero si el índice está contenido en la lista.
+
+	Ejemplo, si la lista tiene 4 elementos y se pide eliminar el índice 9 este método retornará falso porque ese índice no está contenido en la lista.
+	*/
+
+    return false;
+}
